e21219bef8e35e893529539a7d35a0d41804b887
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2020-07-03 13:04:36 -0500

    Fix bug that was adding a day to year 2100

---
+++ app/src/main/java/com/example/deathcountdown/StartUpScreen.java
@@ -0,0 +1,115 @@
+package com.example.deathcountdown;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class StartUpScreen extends AppCompatActivity {
+    private static final Integer INTERVAL = 1000;
+    public File projFile;
+    Button button;
+    TextView textView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.button);
+        textView= (TextView) findViewById(R.id.textView);
+
+        int year = 2080;
+        int month = 7;
+        int day = 6;
+        int hour = 5;
+        int minute = 30;
+        int second = 20;
+
+        try {
+            String dirPath = getFilesDir().getAbsolutePath() + File.separator + "timeLeft";
+            File projDir = new File(dirPath);
+            if (!projDir.exists())
+                projDir.mkdirs();
+            projFile = new File(dirPath + File.separator + "timeLeft.txt");
+            if(projFile.exists()) {
+                projFile.delete();
+            }
+            if(!projFile.exists()) {
+                projFile.createNewFile();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(projFile));
+                String csv = year + "," + month + "," + day +"," + hour + "," + minute + "," + second;
+                writer.write(csv);
+                writer.close();
+            }
+            BufferedReader brTest = new BufferedReader(new FileReader(projFile));
+            String text = brTest.readLine();
+            if(text != null) {
+                String[] strArray = text.split(",");
+                year = Integer.valueOf(strArray[0]);
+                month = Integer.valueOf(strArray[1]);
+                day = Integer.valueOf(strArray[2]);
+                hour = Integer.valueOf(strArray[3]);
+                minute = Integer.valueOf(strArray[4]);
+                second = Integer.valueOf(strArray[5]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final LocalDateTime endDate = LocalDateTime.of(year, month, day, hour, minute, second);
+
+        textView.setText(calculateRemainingTime(LocalDateTime.now(), endDate));
+
+        //This will re-calculate and display the remaining time every second
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setEnabled(false);
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(calculateRemainingTime(LocalDateTime.now(), endDate));
+                        handler.postDelayed(this, INTERVAL);
+                    }
+                }, INTERVAL);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static String calculateRemainingTime(LocalDateTime start, LocalDateTime end)
+    {
+        //calculate each unit of time, then subtract that unit from the end before calculating the next
+        long days = ChronoUnit.DAYS.between(start, end);
+        LocalDateTime newEnd = end.minusDays(days);
+
+        long hours = ChronoUnit.HOURS.between(start, newEnd);
+        newEnd = newEnd.minusHours(hours);
+
+        long minutes = ChronoUnit.MINUTES.between(start, newEnd);
+        newEnd = newEnd.minusMinutes(minutes);
+
+        long seconds = ChronoUnit.SECONDS.between(start, newEnd);
+
+        String difference = String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
+
+        return difference;
+    }
+}
--- app/src/test/java/com/example/deathcountdown/ExampleUnitTest.java
+++ app/src/test/java/com/example/deathcountdown/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package com.example.deathcountdown;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +13,54 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private LocalDateTime startDate;
+    private StartUpScreen screen;
+
+    @Before
+    public void setUp(){
+        screen = new StartUpScreen();
+        startDate = LocalDateTime.of(2020, 6, 25, 12, 00, 0);
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void calculates0DaysLeft() {
+        LocalDateTime endDate = LocalDateTime.of(2020, 6, 25, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("00:00:00:00", days);
+    }
+
+    @Test
+    public void calculates5DaysLeft() {
+        LocalDateTime endDate = LocalDateTime.of(2020, 6, 30, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("05:00:00:00", days);
+    }
+
+    @Test
+    public void calculates365DaysLeft() {
+        LocalDateTime endDate = LocalDateTime.of(2021, 6, 25, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("365:00:00:00", days);
+    }
+
+    @Test
+    public void calculatesDaysLeftForYear2099() {
+        LocalDateTime endDate = LocalDateTime.of(2099, 6, 25, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("28854:00:00:00", days);
+    }
+
+    @Test
+    public void calculatesDaysLeftForYear2100() {
+        LocalDateTime endDate = LocalDateTime.of(2100, 6, 25, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("29219:00:00:00", days);
+    }
+
+    @Test
+    public void calculatesDaysLeftForYear2111() {
+        LocalDateTime endDate = LocalDateTime.of(2111, 6, 25, 12, 00, 0);
+        String days = screen.calculateRemainingTime(startDate, endDate);
+        assertEquals("33236:00:00:00", days);
     }
 }
